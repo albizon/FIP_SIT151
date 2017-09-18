46ae1ab80b2337f19993298f5806bda6829325f5
Alexis BIZON <alexis.kayak@gmail.com>
2017-09-18 19:35:15 +0200

    ajout prototype getkey()

--- TP1/crypter.h
+++ TP1/crypter.h
@@ -3,15 +3,25 @@
 
 
 /*
-@author : 				Alexis BIZON
-@Revision : 			1.0
-@Creation Date : 		18/09/2017
+@author : 		Alexis BIZON
+@Revision : 		1.0
+@Creation Date : 	18/09/2017
 @Function Overview : 	Fonction lisant une phrase saisie au clavier par l'utilisateur commençant et finissant par "
 @params :
-						-char string[] : pointeur vers le tableau contennant la chaine à renvoyer
-@return :				-int : longueur de la chaîne de caractères
+			-char string[] : pointeur vers le tableau contennant la chaine à renvoyer
+@return :		-int : longueur de la chaîne de caractères
 */
 int lirePhrase(char string[]);
+
+/*
+@author : 		Alexis BIZON
+@Revision : 		1.0
+@Creation Date : 	18/09/2017
+@Function Overview : 	Fonction lisant une clef étant un entier compris entre 0 et 256
+@params :		-none
+@return :		-int : la clef
+*/
+int getKey();
 
 
 
