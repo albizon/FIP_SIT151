935d789f9e7873e8dafaf7a7474d76e750356435
Alexis BIZON <alexis.kayak@gmail.com>
2017-09-18 19:24:23 +0200

    Create crypter.h

---
+++ TP1/crypter.h
@@ -0,0 +1,19 @@
+#infndef __CRYPTER_H__
+#define __CRYPTER_H__
+
+
+/*
+@author : 				Alexis BIZON
+@Revision : 			1.0
+@Creation Date : 		18/09/2017
+@Function Overview : 	Fonction lisant une phrase saisie au clavier par l'utilisateur commençant et finissant par "
+@params :
+						-char string[] : pointeur vers le tableau contennant la chaine à renvoyer
+@return :				-int : longueur de la chaîne de caractères
+*/
+int lirePhrase(char string[]);
+
+
+
+
+#endif
