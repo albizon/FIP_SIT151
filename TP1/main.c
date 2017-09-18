9f0d089265ef02faea51423cc58858686ff5b9da
Alexis BIZON <alexis.kayak@gmail.com>
2017-09-18 19:46:59 +0200

    construction du main q1

--- TP1/main.c
+++ TP1/main.c
@@ -15,12 +15,21 @@
 #define YES 1
 #define NO 0
 
-#define STRING_MAX_SIZE 200
+#define STRING_MAX_SIZE 200//Longueur max de la chaîne de caractères
 
 
 int main()
 {
-
+  char string[STRING_MAX_SIZE];//tableau contenant la chaine de caractère
+  int key =0;//clef de cryptage
+  int length =0;//longueur de la chaine de caractères
+  
+  length = lirePhrase(string);//on lit la chaine de caractères
+  
+  key = getKey();//on lit la clef
+  
+  printf("Le message à chiffrer est : %s\n", string);//on affiche la chaine à crypter
+  printf("La clef de chiffrement est : %d\n", key);//on affiche la clef de cryptage
 
   return 0;
 }
