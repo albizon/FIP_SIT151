b38a0d903b6b15e174afdb0a72410dcf17cb27ec
Alexis BIZON <alexis.kayak@gmail.com>
2017-09-18 19:18:35 +0200

    Create main.c

---
+++ TP1/main.c
@@ -0,0 +1,26 @@
+/*
+@Author : Alexis BIZON
+@Revision : 1.0
+@Creation Date : 18/09/2017
+@Project Overview : TP pour chiffrer et déchiffrer une chaine de caractères
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "crypter.h"
+
+#define TRUE 1
+#define FALSE 0
+#define YES 1
+#define NO 0
+
+#define STRING_MAX_SIZE 200
+
+
+int main()
+{
+
+
+  return 0;
+}
